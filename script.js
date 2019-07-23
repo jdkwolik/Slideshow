27aea0538c638c6c7d22c9414a86cbcb81e91609
Joshuah Kwolik <jdkwolik@mac.com>
2019-07-23 18:41:54 -0500

    added slide animation

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ nextButton.addEventListener('click', nextImage);
 prevButton.addEventListener('click', prevImage);
 
 function nextImage(){
+  image.animate([{opacity:'0.1'},{opacity:'1.0'}],{duration:1000,fill:'forwards'});
   if (counter === 4){
     counter = -1;
   }
@@ -15,6 +16,7 @@ function nextImage(){
   image.style.backgroundImage = `url(img/puppy-${counter}.jpg)`
 }
 function prevImage(){
+  image.animate([{opacity:'0.1'},{opacity:'1.0'}],{duration:1000,fill:'forwards'});
   if (counter === 0){
     counter = 5;
   }
